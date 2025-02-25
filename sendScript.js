eed3f31cf89b709fca45cd5fd0c5df60a798108e
mistermonster100 <mistermonster100@gmail.com>
2025-02-25 08:50:54 -0500

    Create sendScript.js

---
+++ sendScript.js
@@ -0,0 +1,19 @@
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+    import { getFirestore, collection, getDocs, setDoc, doc, query, where, updateDoc} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDjSRPgwoeNdSnRleq85mS_mqmV9Tdrkzs",
+      authDomain: "tiger-tutors.firebaseapp.com",
+      projectId: "tiger-tutors",
+      storageBucket: "tiger-tutors.firebasestorage.app",
+      messagingSenderId: "343214157028",
+      appId: "1:343214157028:web:9bf5d0453068e95ddf90f1",
+      measurementId: "G-3ZMT5ERRFR"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+
+//Object.assign(window, {updateSubcategories, findTutors, updateTutor, updateCompetency, getDocIdByEmail});
