1bab58cc0341881c0e0daf5fbd4347813da3f29c
mistermonster100 <mistermonster100@gmail.com>
2025-02-25 08:35:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+    import { getFirestore, collection, getDocs, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+    // 🔹 Replace with your Firebase config
+    const firebaseConfig = {
+      apiKey: "AIzaSyDjSRPgwoeNdSnRleq85mS_mqmV9Tdrkzs",
+      authDomain: "tiger-tutors.firebaseapp.com",
+      projectId: "tiger-tutors",
+      storageBucket: "tiger-tutors.firebasestorage.app",
+      messagingSenderId: "343214157028",
+      appId: "1:343214157028:web:9bf5d0453068e95ddf90f1",
+      measurementId: "G-3ZMT5ERRFR"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
